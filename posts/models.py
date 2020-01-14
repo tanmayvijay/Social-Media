fc8abeb01a473fe62fc67beef7eb60aadeb9fa10
Tanmay Vijay <51825251+tanmayvijay@users.noreply.github.com>
2020-01-14 09:30:00 +0530

    post model done

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,36 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
+
+
+class PublishedManager(models.Manager):
+	def get_queryset(self):
+		return super(PublishedManager, self).get_queryset().filter(status='published')
+
+
+
+class Post(models.Model):
+	STATUS_CHOICES = (('draft', 'Draft'), ('published', 'Published'),)
+	title = models.CharField(max_length=250)
+	slug = models.SlugField(max_length=250, unique_for_date='publish')
+	author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+	body = models.TextField()
+
+	publish = models.DateTimeField(default=timezone.now)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+	status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+	objects = models.Manager()
+	published = PublishedManager()
+	
+	class Meta:
+		ordering = ('-publish',)
+
+	def __str__(self):
+		return self.title + ", " + str(self.publish)
+
+
+	def get_absolute_url(self):
+		return reverse('posts:post_detail', args=[self.pk, self.slug])
--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 # Create your views here.
 def posts_home(request):
-	pass
+	posts = Post.published.all()
+	return render(request, 'posts/posts_home.html', {'posts':posts})
 
 
 def post_detail(request, pk, slug):
-	pass
+	post = get_object_or_404(Post, pk=pk)
+
+	return render(request, 'posts/post_detail.html', {'post':post})

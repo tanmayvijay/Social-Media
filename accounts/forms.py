795ad032be32316d854b2b831f9672f675650b52
Tanmay Vijay <51825251+tanmayvijay@users.noreply.github.com>
2020-01-15 14:45:13 +0530

    added comments to forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,18 +1,21 @@
-from django import forms
-from .models import UserProfile
-from django.contrib.auth.models import User
-from django.core.exceptions import ValidationError
+#imports
+from django import forms # django forms library
+from django.contrib.auth.models import User # django user model
+from django.core.exceptions import ValidationError # for email and password validation
+from .models import UserProfile # custom model from models.py file
 
+# user registration form
 class UserRegistrationForm(forms.ModelForm):
+	# custom input fields
 	password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
 	password2 = forms.CharField(label='Re-Enter Password', widget=forms.PasswordInput)
 
-
+	# meta class
 	class Meta:
 		model = User
 		fields = ('username', 'first_name', 'last_name', 'email', )
 
-
+	# to check for password match
 	def clean_password2(self):
 		cd = self.cleaned_data
 		if cd['password1'] != cd['password2']:
@@ -23,21 +26,21 @@ class UserRegistrationForm(forms.ModelForm):
 
 
 
-
+# profile registration form
 class UserProfileForm(forms.ModelForm):
-
+	# meta class
 	class Meta:
 		model = UserProfile
 		fields = ('mob_no',)
 
 
-
+# login form
 class LoginForm(forms.Form):
 	username = forms.CharField()
 	password = forms.CharField(widget=forms.PasswordInput)
 
 
-
+# edit profile form
 class EditProfileForm(forms.Form):
 	username = forms.CharField()
 	first_name = forms.CharField()
@@ -46,6 +49,7 @@ class EditProfileForm(forms.Form):
 	mob_no = forms.IntegerField()
 
 
+	# to check for unique username
 	def clean_username(self):
 		username = self.cleaned_data['username']
 		if User.objects.filter(username=username).exists():
@@ -53,7 +57,7 @@ class EditProfileForm(forms.Form):
 
 		return username
 
-
+	# to check for unique email
 	def clean_email(self):
 		email = self.cleaned_data['email']
 		if User.objects.filter(email=email).exists():

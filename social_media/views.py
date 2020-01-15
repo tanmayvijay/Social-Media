bc1a023f4fdc8424651665ed07a7d376ead49a68
Tanmay Vijay <51825251+tanmayvijay@users.noreply.github.com>
2020-01-15 15:20:37 +0530

    commented views.py

--- social_media/views.py
+++ social_media/views.py
@@ -1,8 +1,14 @@
+# imports
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse_lazy
 
 
+
+# view for main homepage
 def homepage(request):
+	# if user is loggedd in redirect to posts page
 	if request.user.is_authenticated:
 		return HttpResponseRedirect(reverse_lazy('posts:posts_home'))
+
+	# otherwise show option for login or register
 	return render(request, 'homepage.html')

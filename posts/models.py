0da52e8ae23dc87066285f2b0c859a5fac0e1091
Tanmay Vijay <51825251+tanmayvijay@users.noreply.github.com>
2020-01-15 15:06:16 +0530

    commented models.py

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,4 @@
+# imports
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
@@ -6,16 +7,19 @@ from taggit.managers import TaggableManager
 from slugify import slugify
 # Create your models here.
 
-
+# manager to get all published posts
 class PublishedManager(models.Manager):
 	def get_queryset(self):
 		return super(PublishedManager, self).get_queryset().filter(status='published')
 
 
+# manager to get all draft posts
 class DraftManager(models.Manager):
 	def get_queryset(self):
 		return super(DraftManager, self).get_queryset().filter(status='draft')
 
+
+# main post model
 class Post(models.Model):
 	STATUS_CHOICES = (('draft', 'Draft'), ('published', 'Published'),)
 	title = models.CharField(max_length=250)
@@ -28,28 +32,37 @@ class Post(models.Model):
 	updated = models.DateTimeField(auto_now=True)
 	status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
+	# managers
 	objects = models.Manager()
 	published = PublishedManager()
 	drafts = DraftManager()
 
+	# topics == tags
 	topics = TaggableManager()
 	
+
+	# meta class
 	class Meta:
 		ordering = ('-publish',)
 
+
+	# string representation of posts
 	def __str__(self):
 		return self.title + ", " + str(self.publish)
 
 
+	# to find url for each post
 	def get_absolute_url(self):
 		return reverse('posts:post_detail', args=[self.pk, self.slug])
 
-	
+	# to change post status to published
 	def publish_post(self):
 		self.status = 'published'
 		self.save()
 		return self.get_absolute_url()
 
+
+	# to automatically generate slug for new posts while saving
 	def save(self, *args, **kwargs):
 		self.slug = slugify(self.title + str(self.created))
 		super(Post, self).save(*args, **kwargs)

49191a524f5c6e2cf9af6064cae41c6a6cd3154e
Tanmay Vijay <51825251+tanmayvijay@users.noreply.github.com>
2020-01-15 14:40:25 +0530

    commented accounts/admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile # importing models to be shown in admin panel
 # Register your models here.
-admin.site.register(UserProfile)
+admin.site.register(UserProfile) # registering UserProfile model to show in admin

4282a8394561729182699375ce166b21f912e463
Tanmay Vijay <51825251+tanmayvijay@users.noreply.github.com>
2020-01-15 14:46:33 +0530

    added comments to models

--- accounts/models.py
+++ accounts/models.py
@@ -1,14 +1,16 @@
+#imports
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
+# user profile model
 class UserProfile(models.Model):
+	# each profile is related to a user field
 	user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE)
 	mob_no = models.IntegerField()
 	following = models.ManyToManyField("self", related_name='followers', blank=True, symmetrical=False)
-	# topics you like to know abt foriegn key
-	# topics you know fk
 
-
+	# string representation of user profile
 	def __str__(self):
 		return self.user.email
 

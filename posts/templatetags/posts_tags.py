b2ccd58bf3c7fecaa059c41ec2de68d9dea0dc03
Tanmay Vijay <51825251+tanmayvijay@users.noreply.github.com>
2020-01-15 15:01:08 +0530

    commented post tags

--- posts/templatetags/posts_tags.py
+++ posts/templatetags/posts_tags.py
@@ -1,18 +1,21 @@
+# imports
 from django import template
-from ..models import Post
 from django.utils.safestring import mark_safe
 import markdown
+from ..models import Post
+
 
 
 register = template.Library()
 
+# # to find the no of published posts
+# @register.simple_tag
+# def total_posts():
+# 	return Post.published.count()
 
-@register.simple_tag
-def total_posts():
-	return Post.published.count()
 
 
-
+# to apply markdown to posts
 @register.filter(name='markdown')
 def markdown_format(text):
 	return mark_safe(markdown.markdown(text))

8c695a0ad38d765ead95ab761e848aca1f72fe44
Tanmay Vijay <51825251+tanmayvijay@users.noreply.github.com>
2020-01-15 15:08:51 +0530

    commented urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -1,16 +1,26 @@
+# imports
 from django.urls import path
 from . import views
 
-
+# giving app name to identify urls
 app_name = 'posts'
 
+
+# list of urls after domain/posts/
 urlpatterns = [
+	# posts home page
 	path('', views.posts_home, name='posts_home'),
+	# to view all topics
 	path('topics/', views.topics_view, name='topics_view'),
+	# to view posts of specific topic
 	path('topics/<slug:topic_slug>', views.posts_home, name='posts_by_topic'),
+	# to create a new post
 	path('new/', views.new_post_view, name='new_post'),
+	# to review draft posts of user (only self)
 	path('drafts/', views.draft_posts, name='draft_posts'),
+	# to view a post
 	path('<int:pk>-<slug:slug>/', views.post_detail, name='post_detail'),
+	# to edit a self post
 	path('<int:pk>-<slug:slug>/edit', views.edit_post, name='edit_post'),
 
 
